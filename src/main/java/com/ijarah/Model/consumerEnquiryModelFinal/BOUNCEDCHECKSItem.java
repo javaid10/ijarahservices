36571cad2319c18ff3b7e104b0511676c823cdb8
Hasnain Sabir <hasnain.sabir@ijarah.sa>
2023-02-08 18:21:51 +0300

    Add Missing Modal classes

---
+++ src/main/java/com/ijarah/Model/consumerEnquiryModelFinal/BOUNCEDCHECKSItem.java
@@ -0,0 +1,63 @@
+package com.ijarah.Model.consumerEnquiryModelFinal;
+
+import com.google.gson.annotations.SerializedName;
+
+
+public class BOUNCEDCHECKSItem{
+
+	@SerializedName("BC_CHECK_NO")
+	private String bCCHECKNO;
+
+	@SerializedName("BC_STAT")
+	private String bCSTAT;
+
+	@SerializedName("BC_CRDTR")
+	private String bCCRDTR;
+
+	@SerializedName("BC_PRD")
+	private String bCPRD;
+
+	@SerializedName("BC_ORIG_AMT")
+	private String bCORIGAMT;
+
+	@SerializedName("BC_CUB")
+	private String bCCUB;
+
+	@SerializedName("BC_SETTLD_DATE")
+	private String bCSETTLDDATE;
+
+	@SerializedName("BC_LOAD_DT")
+	private String bCLOADDT;
+
+	public String getBCCHECKNO(){
+		return bCCHECKNO;
+	}
+
+	public String getBCSTAT(){
+		return bCSTAT;
+	}
+
+	public String getBCCRDTR(){
+		return bCCRDTR;
+	}
+
+	public String getBCPRD(){
+		return bCPRD;
+	}
+
+	public String getBCORIGAMT(){
+		return bCORIGAMT;
+	}
+
+	public String getBCCUB(){
+		return bCCUB;
+	}
+
+	public String getBCSETTLDDATE(){
+		return bCSETTLDDATE;
+	}
+
+	public String getBCLOADDT(){
+		return bCLOADDT;
+	}
+}
---
+++ src/main/java/com/ijarah/Model/consumerEnquiryModelFinal/JUDGEMENTSItem.java
@@ -0,0 +1,83 @@
+package com.ijarah.Model.consumerEnquiryModelFinal;
+
+import com.google.gson.annotations.SerializedName;
+
+public class JUDGEMENTSItem{
+
+	@SerializedName("EJ_ORIG_CLAIM_AMT")
+	private String eJORIGCLAIMAMT;
+
+	@SerializedName("EJ_EXEC_TYPE")
+	private String eJEXECTYPE;
+
+	@SerializedName("EJ_STATUS")
+	private String eJSTATUS;
+
+	@SerializedName("EJ_SETTLE_DATE")
+	private String eJSETTLEDATE;
+
+	@SerializedName("EJ_ENFORCE_DATE")
+	private String eJENFORCEDATE;
+
+	@SerializedName("EJ_DATE_LOADED")
+	private String eJDATELOADED;
+
+	@SerializedName("EJ_CITY")
+	private String eJCITY;
+
+	@SerializedName("EJ_CLAIM_AMT")
+	private String eJCLAIMAMT;
+
+	@SerializedName("EJ_RES_NUMBER")
+	private String eJRESNUMBER;
+
+	@SerializedName("EJ_CASE_NUMBER")
+	private String eJCASENUMBER;
+
+	@SerializedName("EJ_COURT_CODE")
+	private String eJCOURTCODE;
+
+	public String getEJORIGCLAIMAMT(){
+		return eJORIGCLAIMAMT;
+	}
+
+	public String getEJEXECTYPE(){
+		return eJEXECTYPE;
+	}
+
+	public String getEJSTATUS(){
+		return eJSTATUS;
+	}
+
+	public String getEJSETTLEDATE(){
+		return eJSETTLEDATE;
+	}
+
+	public String getEJENFORCEDATE(){
+		return eJENFORCEDATE;
+	}
+
+	public String getEJDATELOADED(){
+		return eJDATELOADED;
+	}
+
+	public String getEJCITY(){
+		return eJCITY;
+	}
+
+	public String getEJCLAIMAMT(){
+		return eJCLAIMAMT;
+	}
+
+	public String getEJRESNUMBER(){
+		return eJRESNUMBER;
+	}
+
+	public String getEJCASENUMBER(){
+		return eJCASENUMBER;
+	}
+
+	public String getEJCOURTCODE(){
+		return eJCOURTCODE;
+	}
+}
